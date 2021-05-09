bfbf9528ba2fbdb94f3c5c5487af1ac0f083c93e
Daniel Vega <98dvega@gmail.com>
2021-05-09 17:45:03 -0500

    intro

--- public/js/intro.js
+++ public/js/intro.js
@@ -46,11 +46,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // delete black intro when clicks HEROKU
 heroku.addEventListener('click', () => {
+    heroku.style.transition = 'transform 0.2s'
+    heroku.style.transform = 'scale(0)'
+    container.style.border = 'solid 2px black'
+    setTimeout(letsGetStarted, 1500)
+})
+
+const letsGetStarted = () => {
     container.style.border = 'solid 2px white'
     heroku.style.background = 'skyblue'
     body().style.background = 'transparent'
     heroku.style.boxShadow = '#33339983 0px 3px 6px 9px, var(--blueShadow)'
-})
+    heroku.style.transition = 'transform 0.1s'
+    heroku.style.transform = 'scale(1)'
+}
 
 
 // end of INTRO
---
+++ public/js/osu!.js
@@ -0,0 +1,23 @@
+// ANTHOLOGY OF ELEMENTAL FUNCTIONS
+
+    // getElementById => getE
+    const getE = id => {
+        return document.getElementById(id)
+    }
+    
+        // document.body > body
+    const body = () => {
+        return document.body
+    }
+    
+        // document.body.addEventListener => bodyEvent
+    const bodyEvt = (evt, fn) => {
+        return body().addEventListener(evt, fn)
+    }
+    
+        //console log
+    const clg = any => {
+        return console.log(any)
+    }
+    
+    // end of ANTHOLOGY OF ELEMENTAL FUNCTIONS 
